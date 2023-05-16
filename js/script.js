bb08afb260d56d80be8da0670fe91b83f2c54732
Magdalena Bień <bien@magda.krakow.pl>
2023-05-16 07:35:31 +0200

    Add active link

--- js/script.js
+++ js/script.js
@@ -7,28 +7,34 @@
 
     const titleClickHandler = function(event)
     {
-    console.log('Link was clicked!');
-  
-    /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
+      const clickedElement = this;
+      console.log('Link was clicked!');
+    
+      /* [DONE] remove class 'active' from all article links  */
+      const activeLinks = document.querySelectorAll('.titles a.active');
+      for(let activeLink of activeLinks)
+      {
+        activeLink.classList.remove('active');
+      }
+    
+      /* [IN PROGRESS] add class 'active' to the clicked link */
+      console.log('clickedElement:', clickedElement);
+      console.log('clickedElement (with plus): ' + clickedElement);
+      clickedElement.classList.add('active');
+    
+      /* [DONE] remove class 'active' from all articles */
+      const activeArticles = document.querySelectorAll('article');
+      for(let activeArticle of activeArticles)
+      {
+        activeArticle.classList.remove('active'); 
+      }   
+    
+      /* get 'href' attribute from the clicked link */
+    
+      /* find the correct article using the selector (value of 'href' attribute) */
+    
+      /* add class 'active' to the correct article */
     }
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('article');
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active'); 
-    }   
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
-  }
   
     const links = document.querySelectorAll('.titles a');
     
