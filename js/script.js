18911ab60fe461e5e622d40972a96a5aeedcbc14
Magdalena Bień <bien@magda.krakow.pl>
2023-05-16 08:03:03 +0200

    Complete titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
-/* document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  }); 
-  */
-
-
     const titleClickHandler = function(event)
     {
+      event.preventDefault();
       const clickedElement = this;
       console.log('Link was clicked!');
     
@@ -17,9 +11,8 @@
         activeLink.classList.remove('active');
       }
     
-      /* [IN PROGRESS] add class 'active' to the clicked link */
+      /* [DONE] add class 'active' to the clicked link */
       console.log('clickedElement:', clickedElement);
-      console.log('clickedElement (with plus): ' + clickedElement);
       clickedElement.classList.add('active');
     
       /* [DONE] remove class 'active' from all articles */
@@ -29,11 +22,17 @@
         activeArticle.classList.remove('active'); 
       }   
     
-      /* get 'href' attribute from the clicked link */
+      /* [DONE] get 'href' attribute from the clicked link */
+      const articleSelector=clickedElement.getAttribute('href');
+      console.log('articleSelector:', articleSelector);
+
+      /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+      const targetArticle=document.querySelector(articleSelector);
+      console.log('targetArticle:', targetArticle);      
     
-      /* find the correct article using the selector (value of 'href' attribute) */
-    
-      /* add class 'active' to the correct article */
+      /* [DONE] add class 'active' to the correct article */
+      targetArticle.classList.add('active');
+
     }
   
     const links = document.querySelectorAll('.titles a');
